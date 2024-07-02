a09703ce23df6932937e35d0257de10c25226ca2
huangyukai <wekohyk@gmail.com>
2024-07-02 17:59:38 +0800

    chore: remove unused tailwind CSS files

--- src/assets/style/index.ts
+++ src/assets/style/index.ts
@@ -2,5 +2,3 @@ import '@unocss/reset/normalize.css';
 import '@unocss/reset/sanitize/sanitize.css';
 import '@unocss/reset/sanitize/assets.css';
 import '@unocss/reset/eric-meyer.css';
-import '@unocss/reset/tailwind.css';
-import '@unocss/reset/tailwind-compat.css';
