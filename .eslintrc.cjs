db96fc4bd3f336cd6c4a6bdd1374964c95dbdfbf
huangyukai <wekohyk@gmail.com>
2024-07-02 17:04:22 +0800

    chore: update eslint configuration to use 'unocss' instead of '@unocss'

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,7 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
     'prettier',
-    '@unocss',
+    'unocss',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
